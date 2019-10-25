c56c14be9b1797e8e89de4ed1d6688f1971245b3
Catrinici <catrinici.antonina@gmail.com>
2019-10-24 20:12:04 -0700

    survey form initial commit

---
+++ survey_form/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, '/views')));
+app.set('view engine', 'ejs');
+
+// I will have /routes/index.js handle all of our routing
+require('./routes/index.js')(app);
+
+app.listen(3000, function() {
+    console.log('listening on port 3000!');
+});
