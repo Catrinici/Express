5aaaedbd8b8a665509b08a64d2c9891d8c4ef960
Catrinici <catrinici.antonina@gmail.com>
2019-10-24 22:44:43 -0700

    final commit

--- great_number_game/routes/index.js
+++ great_number_game/routes/index.js
@@ -1,7 +1,37 @@
 module.exports = function Route(app) {
-    app.get('/', function(req, res) {
-        res.render('index');
-    });
+
+    app.get("/", (req, res) => {
+
+        if (req.session.number) {} else {
+            req.session.number = Math.floor(Math.random() * 100) + 1
+        }
 
 
-};
+        context = {
+            "number": req.session.number,
+            "message": req.session.message,
+            "replay": req.session.replay
+
+        }
+        res.render('index', context)
+    })
+    app.post("/guess", (req, res) => {
+        req.session.guess = req.body.guess
+        if (req.session.guess > req.session.number) {
+            req.session.message = "Guess too high"
+        }
+        if (req.session.guess < req.session.number) {
+            req.session.message = "Guess too low"
+        }
+        if (req.session.guess == req.session.number) {
+            req.session.message = "You guessed the number!"
+            req.session.replay = 1
+        }
+        res.redirect("/")
+    })
+    app.get("/reset", (req, res) => {
+        req.session.destroy()
+        res.redirect("/")
+    })
+
+}
--- great_number_game/server.js
+++ great_number_game/server.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const app = express();
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, '/views')));
-app.set('view engine', 'ejs');
 
-// I will have /routes/index.js handle all of our routing
+app.set('view engine', 'ejs');
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: true }
+    }))
+    // I will have /routes/index.js handle all of our routing
 require('./routes/index.js')(app);
+
+
+
+
+
 
 app.listen(8000, function() {
     console.log('listening on port 8000!');
